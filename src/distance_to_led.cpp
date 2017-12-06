0eebfa9ff2f42b4e34f1801048b2f3f3928ef7d0
aloecandy@gmail.com <aloecandy@gmail.com>
2017-12-06 22:30:10 +0900

    test8

--- src/distance_to_led.cpp
+++ src/distance_to_led.cpp
@@ -7,23 +7,18 @@ ros::ServiceClient sc;
 pi::led srv;
 void callback(const sensor_msgs::Range::ConstPtr& msg)
 {
-  long int id=1;
-  long int color=255;
-  long int brightness=255;
-  
   ROS_INFO("distance = %fm", msg->range); 
 //  int num=((msg->range)*20;
   for(int i=0;i<8;i++){
     if(i==8)  break;
-    id=i;
-    srv.request.led_id=id;
-    srv.request.color=color;
-    srv.request.brightness=brightness;
+    srv.request.led_id=i;
+    srv.request.color=255;
+    srv.request.brightness=255;
     if((srv.request.led_id!=srv.response.led_id)||(srv.request.color!=srv.response.color)||(srv.request.brightness!=srv.response.brightness)){
         if(sc.call(srv))
         {
-            ROS_INFO("req : id=%d, color=%d, brightness=%d",srv.request.led_id,srv.request.color,srv.request.brightness);
-            ROS_INFO("res : id=%d, color=%d, brightness=%d",srv.response.led_id,srv.response.color,srv.response.brightness);
+            ROS_INFO("req : id=%d, color=%ld, brightness=%d",srv.request.led_id,srv.request.color,srv.request.brightness);
+            ROS_INFO("res : id=%d, color=%ld, brightness=%d",srv.response.led_id,srv.response.color,srv.response.brightness);
         }
         else{
             ROS_ERROR("Failed to call service");
