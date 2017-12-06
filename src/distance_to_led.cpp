9ada27fce8f853fee63903c0600601d7d5e961eb
aloecandy@gmail.com <aloecandy@gmail.com>
2017-12-06 22:36:18 +0900

    test10

--- src/distance_to_led.cpp
+++ src/distance_to_led.cpp
@@ -8,11 +8,11 @@ pi::led srv;
 void callback(const sensor_msgs::Range::ConstPtr& msg)
 {
   ROS_INFO("distance = %fm", msg->range); 
-  int num=((msg->range)*20;
+  int num=(msg->range)*20;
   for(int i=0;i<num;i++){
     if(i==8)  break;
     srv.request.led_id=i;
-    srv.request.color=255;
+    srv.request.color=(i<num)? 255:(255<<8);
     srv.request.brightness=255;
     if((srv.request.led_id!=srv.response.led_id)||(srv.request.color!=srv.response.color)||(srv.request.brightness!=srv.response.brightness)){
         if(sc.call(srv))
